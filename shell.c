55f69eea8bb27a4a3c5b170587bf195c7153b1dd
rrubaszek <robert.rubaszek@gmail.com>
2025-04-07 19:34:46 +0200

    updated readme

--- shell.c
+++ shell.c
@@ -1,3 +1,5 @@
+// Extended shell with input/output/error redirection, pipe chaining, and job control
+
 #include <fcntl.h>
 #include <stdio.h> 
 #include <string.h> 
@@ -6,475 +8,238 @@
 #include <signal.h>
 #include <sys/types.h> 
 #include <sys/wait.h> 
+#include <errno.h>
 
 #define clear() printf("\033[H\033[J") 
-
 #define MAX 100
 
-void initialize_shell_window()
-{
+void initialize_shell_window() {
     clear(); 
     printf("LSH SHELL\n");
-    printf("CURRENT USER: @%s", getenv("USER")); 
-    printf("\n"); 
+    printf("CURRENT USER: @%s\n", getenv("USER")); 
 }
 
-void sig_handler()
-{
+void sig_handler(int sig) {
     printf("\n");
+    fflush(stdout);
 }
 
-void background_handler()
-{
-    wait(NULL);
+void background_handler(int sig) {
+    waitpid(-1, NULL, WNOHANG);
 }
 
-int command_lenght(char** command)
-{
+int command_length(char** command) {
     int i = 0;
-
-	while(command[i] != NULL)
-	{
-		i++;
-	}
-	return i;
+    while (command[i] != NULL) i++;
+    return i;
 }
 
-char** split_command(char* input)
-{
-    char* p = strtok(input, " ");
-	char** s = malloc(1024 * sizeof(char*));
-	int i = 0;
-	while(p != NULL)
-	{
-		s[i++] = strdup(p);
-		p = strtok(NULL, " ");
-	}
+void free_command(char** command) {
+    for (int i = 0; command[i]; i++) free(command[i]);
+    free(command);
+}
+
+char** split_command(const char* input) {
+    char* dup = strdup(input);
+    char* p = strtok(dup, " \t\n");
+    char** s = malloc(1024 * sizeof(char*));
+    int i = 0;
+    while (p != NULL) {
+        s[i++] = strdup(p);
+        p = strtok(NULL, " \t\n");
+    }
+    s[i] = NULL;
+    free(dup);
+    return s;
+}
+
+char** split_delim(char* input, const char* delim) {
+    char* p = strtok(input, delim);
+    char** s = malloc(1024 * sizeof(char*));
+    int i = 0;
+    while (p != NULL) {
+        s[i++] = strdup(p);
+        p = strtok(NULL, delim);
+    }
     s[i] = NULL;
     return s;
 }
 
-char** split_pipes(char* input)
-{
-	char* p = strtok(input, "|");
-	char** s = malloc(1024 * sizeof(char*));
-	int i = 0;
-	while(p != NULL)
-	{
-		s[i++] = strdup(p);
-		p = strtok(NULL, "|");
-	}
-    s[i] = NULL;
-	return s;
-}
-
-char** split_out(char* input)
-{
-    char* p = strtok(input, " > ");
-	char** s = malloc(1024 * sizeof(char*));
-	int i = 0;
-	while(p != NULL)
-	{
-		s[i++] = strdup(p);
-		p = strtok(NULL, " > ");
-	}
-    s[i] = NULL;
-	return s;   
-}
-
-char** split_in(char* input)
-{
-    char* p = strtok(input, " < ");
-	char** s = malloc(1024 * sizeof(char*));
-	int i = 0;
-	while(p != NULL)
-	{
-		s[i++] = strdup(p);
-		p = strtok(NULL, " < ");
-	}
-    s[i] = NULL;
-	return s;
-}
-
-char** split_err(char* input)
-{
-    char* p = strtok(input, " 2> ");
-	char** s = malloc(1024 * sizeof(char*));
-	int i = 0;
-	while(p != NULL)
-	{
-		s[i++] = strdup(p);
-		p = strtok(NULL, " 2> ");
-	}
-    s[i] = NULL;
-	return s;
-}
-void execute(char** parsed, int bg_flag)
-{
-    pid_t id;
-   
-    id = fork();
-
-    if(id == 0)
-    {
+void execute(char** args, int bg_flag) {
+    pid_t pid = fork();
+    if (pid == 0) {
         signal(SIGINT, SIG_DFL);
-        
         signal(SIGTSTP, SIG_DFL);
-
-        int k = execvp(parsed[0], parsed);
-
+        execvp(args[0], args);
+        perror("execvp failed");
         exit(EXIT_FAILURE);
-    }
-    else if(id < 0)
-    {
-        perror("LSH");
-    }
-    else
-    {
-        if(bg_flag == 0)
-        {
-            wait(NULL);
-        }
-        else
-        {
+    } else if (pid > 0) {
+        if (!bg_flag) {
+            waitpid(pid, NULL, 0);
+        } else {
             signal(SIGCHLD, background_handler);
         }
+    } else {
+        perror("fork failed");
     }
-
 }
 
-void execute_pipe(char** command)
-{
-    int fd[2];
-    int tempin = dup(STDIN_FILENO);			
-	int tempout = dup(STDOUT_FILENO);
+void execute_redirection(char* buffer, int bg_flag) {
+    char* cmd = strdup(buffer);
+    int in_redirect = 0, out_redirect = 0, err_redirect = 0;
+    char *in_file = NULL, *out_file = NULL, *err_file = NULL;
 
-    fd[0] = dup(tempin);
+    char* input_part = strtok(cmd, ">\n");
+    char* output_part = strstr(buffer, ">");
+    char* error_part = strstr(buffer, "2>");
+    char* input_redir = strstr(buffer, "<");
 
-    for(int i = 0; i < command_lenght(command); i++)
-    {
-        char** s_command = split_command(command[i]);
+    if (input_redir) {
+        in_redirect = 1;
+        *input_redir = 0;
+        in_file = strtok(input_redir + 1, " \t");
+    }
+    if (error_part) {
+        err_redirect = 1;
+        *error_part = 0;
+        err_file = strtok(error_part + 2, " \t");
+    }
+    if (output_part && (!err_redirect || output_part < error_part)) {
+        out_redirect = 1;
+        *output_part = 0;
+        out_file = strtok(output_part + 1, " \t");
+    }
 
-        dup2(fd[0], STDIN_FILENO);
-		close(fd[0]);
-
-        if(i == command_lenght(command) - 1)
-        {
-            fd[1] = dup(tempout);
+    char** args = split_command(input_part);
+    pid_t pid = fork();
+    if (pid == 0) {
+        if (in_redirect) {
+            int fd = open(in_file, O_RDONLY);
+            if (fd == -1) { perror("open input"); exit(1); }
+            dup2(fd, STDIN_FILENO);
+            close(fd);
         }
-        else
-        {
-            int fd_copy[2];
-		    pipe(fd_copy);
-		    fd[1] = fd_copy[1];
-		    fd[0] = fd_copy[0];
+        if (out_redirect) {
+            int fd = open(out_file, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+            if (fd == -1) { perror("open output"); exit(1); }
+            dup2(fd, STDOUT_FILENO);
+            close(fd);
         }
-        
+        if (err_redirect) {
+            int fd = open(err_file, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+            if (fd == -1) { perror("open error"); exit(1); }
+            dup2(fd, STDERR_FILENO);
+            close(fd);
+        }
+        signal(SIGINT, SIG_DFL);
+        signal(SIGTSTP, SIG_DFL);
+        execvp(args[0], args);
+        perror("execvp");
+        exit(1);
+    } else if (pid > 0) {
+        if (!bg_flag) waitpid(pid, NULL, 0);
+        else signal(SIGCHLD, background_handler);
+    } else {
+        perror("fork");
+    }
 
-        dup2(fd[1], STDOUT_FILENO);
-		close(fd[1]);
+    free(args);
+    free(cmd);
+}
 
-        pid_t id = fork();
-        if(id == 0)
-        {
+void execute_pipe(char** commands) {
+    int n = command_length(commands);
+    int tempin = dup(STDIN_FILENO);
+    int tempout = dup(STDOUT_FILENO);
+    int fdin = dup(STDIN_FILENO);
+
+    for (int i = 0; i < n; i++) {
+        int fd[2];
+        pipe(fd);
+
+        dup2(fdin, STDIN_FILENO);
+        close(fdin);
+
+        if (i == n - 1) {
+            dup2(tempout, STDOUT_FILENO);
+        } else {
+            dup2(fd[1], STDOUT_FILENO);
+        }
+
+        close(fd[1]);
+        fdin = fd[0];
+
+        pid_t pid = fork();
+        if (pid == 0) {
+            char** args = split_command(commands[i]);
             signal(SIGINT, SIG_DFL);
-            
             signal(SIGTSTP, SIG_DFL);
-
-            execvp(s_command[0], s_command);
-
+            execvp(args[0], args);
+            perror("execvp failed");
             exit(EXIT_FAILURE);
+        } else if (pid < 0) {
+            perror("fork");
+        } else {
+            waitpid(pid, NULL, 0);
         }
-        wait(NULL);
     }
 
     dup2(tempin, STDIN_FILENO);
-	dup2(tempout, STDOUT_FILENO);
-	close(tempin);
-	close(tempout);
+    dup2(tempout, STDOUT_FILENO);
+    close(tempin);
+    close(tempout);
 }
 
-/*
-void execute_pipe_TODO(char** command)
-{
-    int tempin = dup(0);			
-	int tempout = dup(1);			
-	int flag = 0;
-	int fdin = 0, fdout;
-
-	for(int j = 0; j < command_lenght(command); j++)
-	{
-		
-		if(strcmp(command[j], "<") == 0)
-		{
-			fdin = open(command[j + 1], O_RDONLY);
-			flag += 2;
-		}
-	}
-
-	if(!fdin)
-    {
-        fdin = dup(tempin);
-    }
-	
-    int id;
-    for(int i = 0; i < command_lenght(command); i++)
-    {
-        char** s_command = split_command(command[i]);
-
-        dup2(fdin, 0);
-		close(fdin);
-		if(i == command_lenght(command) - 3 && strcmp(command[i + 1], ">") == 0)
-		{	
-			if((fdout = open(command[i + 2], O_WRONLY)))
-            {
-                i++;
-            }
-		}
-		else if(i == command_lenght(command) - flag - 1)
-        {
-            fdout = dup(tempout);
-        }
-		else
-        {
-            printf("standard case\n");
-            int fd[2];
-		    pipe(fd);
-		    fdout = fd[1];
-		    fdin = fd[0];
-        }
-
-        dup2(fdout, 1);
-		close(fdout);
-
-        id = fork();
-        if(id == 0)
-        {
-            execvp(s_command[0], s_command);
-
-            exit(EXIT_FAILURE);
-        }
-        wait(NULL);
-    }
-
-    dup2(tempin, 0);
-	dup2(tempout, 1);
-	close(tempin);
-	close(tempout);
-
-}
-*/
-
-int main()
-{
-    char* buffer;
-    char** parsed;
-    size_t buffsize = 1024;
-    char* commands[2];
-
-    commands[0] = "exit";
-    commands[1] = "cd";
-
+int main() {
+    char* buffer = NULL;
+    size_t buffsize = 0;
     initialize_shell_window();
-
     signal(SIGINT, sig_handler);
     signal(SIGTSTP, sig_handler);
 
-    while(1)
-    {
-        int i = 0;
-        int bg_flag = 0;
-        int pipe_flag = 0;
-        int flag_itr = 0;
-        int in_flag = 0;
-        int out_flag = 0;
-        int err_flag = 0;
-
-        
-    
+    while (1) {
         printf(">> ");
-
-        buffer = (char*)malloc(buffsize * sizeof(char));
-
-        if(getline(&buffer, &buffsize, stdin) == -1)
-        {
-            if(feof(stdin))
-            {
-                free(buffer);
-                exit(EXIT_SUCCESS);
-            }
-            else
-            {
-                free(buffer);
-                perror("error while reading the line from stdin\n");
-                exit(EXIT_FAILURE);
-            }
+        if (getline(&buffer, &buffsize, stdin) == -1) {
+            if (feof(stdin)) break;
+            perror("getline failed");
+            continue;
         }
 
-        int size = strlen(buffer);
+        size_t size = strlen(buffer);
+        if (size > 0 && buffer[size - 1] == '\n') buffer[size - 1] = '\0';
 
-        if(buffer[size-1] == '\n')
-        {
-            buffer[size-1] = '\0';
-        }
-
-        if(buffer[size-2] == '&')
-        {
+        int bg_flag = 0;
+        if (size >= 2 && buffer[size - 2] == '&') {
             bg_flag = 1;
-            buffer[size-2] = '\0';
+            buffer[size - 2] = '\0';
         }
 
-        if(strcmp(buffer, commands[0]) == 0)
-        {
+        if (strcmp(buffer, "exit") == 0) {
             printf("Shutting down...\n");
             break;
         }
 
-        while(buffer[flag_itr] != '\0')
-        {
-            if(buffer[flag_itr] == '|')
-			{	
-				pipe_flag = 1;
-				break;
-			}
-			flag_itr++;
+        if (strncmp(buffer, "cd", 2) == 0) {
+            char** args = split_command(buffer);
+            if (args[1]) chdir(args[1]);
+            else chdir(getenv("HOME"));
+            free_command(args);
+            continue;
         }
 
-        flag_itr = 0;
-        while(buffer[flag_itr] != '\0')
-        {
-            if(buffer[flag_itr] == '>')
-			{	
-				out_flag = 1;
-				break;
-			}
-			flag_itr++;
+        if (strchr(buffer, '|')) {
+            char** piped = split_delim(buffer, "|");
+            execute_pipe(piped);
+            free_command(piped);
+        } else if (strchr(buffer, '<') || strstr(buffer, ">") || strstr(buffer, "2>")) {
+            execute_redirection(buffer, bg_flag);
+        } else {
+            char** args = split_command(buffer);
+            execute(args, bg_flag);
+            free_command(args);
         }
-
-        flag_itr = 0;
-        while(buffer[flag_itr] != '\0')
-        {
-            if(buffer[flag_itr] == '<')
-			{	
-				in_flag = 1;
-				break;
-			}
-			flag_itr++;
-        }
-
-        flag_itr = 0;
-        while(buffer[flag_itr] != '\0')
-        {
-            if(buffer[flag_itr] == '2' && buffer[flag_itr + 1] == '>')
-			{	
-				err_flag = 1;
-				break;
-			}
-			flag_itr++;
-        }
-
-        if(pipe_flag)
-        {
-            parsed = split_pipes(buffer);
-            execute_pipe(parsed);
-            free(parsed);
-        }
-        else if(out_flag)
-        {
-            parsed = split_out(buffer);
-
-            char** temp = split_command(parsed[0]);
-            
-            pid_t id = fork();
-
-            if(id == 0)
-            {
-                signal(SIGINT, SIG_DFL);
-
-                int file = open(parsed[1], O_WRONLY | O_CREAT, 0777);
-                dup2(file, STDOUT_FILENO);
-                close(file);    
-                execvp(temp[0], temp);
-            }
-            else
-            {
-                wait(NULL);
-            }
-            
-            free(temp);
-            free(parsed);
-
-        }
-        else if(in_flag)
-        {
-            parsed = split_in(buffer);
-
-            char** temp = split_command(parsed[0]);
-
-            pid_t id = fork();
-
-            if(id == 0)
-            {
-                signal(SIGINT, SIG_DFL);
-
-                int file = open(parsed[1], O_RDONLY);
-                dup2(file, STDIN_FILENO);
-                close(file);
-                execvp(temp[0], temp);
-            }
-            else
-            {
-                wait(NULL);
-            }
-
-            free(temp);
-            free(parsed);
-
-        }
-        else if (err_flag)
-        {
-            parsed = split_err(buffer);
-
-            char** temp = split_command(parsed[0]);
-        
-            pid_t id = fork();
-
-            if(id == 0)
-            {
-                signal(SIGINT, SIG_DFL);
-
-                int file = open(parsed[1], O_WRONLY | O_CREAT, 0777);
-                dup2(file, STDERR_FILENO);
-                close(file);
-                execvp(temp[0], temp);
-            }
-            else
-            {
-                wait(NULL);
-            }
-
-            free(temp);
-            free(parsed);
-
-        }
-        else
-        {
-            parsed = split_command(buffer);
-
-            if(strcmp(parsed[0], commands[1]) == 0)
-            {
-                chdir(parsed[1]);
-            }
-            else
-            {
-                execute(parsed, bg_flag);
-            }
-
-            free(parsed);
-
-        }
-        free(buffer);
     }
 
+    free(buffer);
     return 0;
 }
